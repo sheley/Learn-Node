23b10a9b97113baf2f64373b26fb9aecfa0ea287
Michele Guido <michele.guido@ada.com>
2017-09-18 12:14:54 +0200

    Everything through video 13.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -14,3 +14,9 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // query DB for list of all stores
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores})
+}
