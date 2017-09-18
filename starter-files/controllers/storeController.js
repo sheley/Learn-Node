a6d7ef503c5b48b65d1bed812b36df3e5e5fba93
Michele Guido <michele.guido@ada.com>
2017-09-18 11:50:43 +0200

    Everything through video 12.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -10,8 +10,7 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  debugger;
-  res.redirect('/');
+  const store = await (new Store(req.body).save());
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
+  res.redirect(`/store/${store.slug}`);
 };
