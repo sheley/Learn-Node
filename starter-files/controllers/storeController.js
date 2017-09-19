8ab4c61b39208fab6c60b299ebe2f7092be40806
Michele Guido <michele.guido@ada.com>
2017-09-19 11:55:18 +0200

    Through video 18.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store')
+const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid')
+
+const multerOptions = {
+  // where will the file be stored
+  // what type of files are allowed
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true)
+    } else {
+      next({ message: 'That file type is not allowed.'}, false)
+    }
+  }
+}
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -9,7 +26,27 @@ exports.addStore = (req, res) => {
   res.render('editStore', {title: 'Add store'})
 }
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there's no new file to resize
+  if (!req.file) {
+    next(); // skip to next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1]
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  // now resize
+  const photo = await jimp.read(req.file.buffer)
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`)
+  // once we have written the photo to the file system keep going
+  next();
+}
+
 exports.createStore = async (req, res) => {
+  // 
   const store = await (new Store(req.body).save());
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`);
@@ -23,6 +60,7 @@ exports.getStores = async (req, res) => {
 
 exports.editStore = async  (req, res) => {
   // find store with given id
+  console.log(req.params)
   const store = await Store.findOne({ _id: req.params.id })
   // confirm the user is the owner of the store
   // render out the edit form so user can update the store
@@ -31,7 +69,9 @@ exports.editStore = async  (req, res) => {
 
 exports.updateStore = async (req, res) => {
   // set location data to be a point
-  req.body.location.type = 'Point';
+  if (req.body.location) {
+    req.body.location.type = 'Point';
+  }
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // returns the new store instead of the old one
     runValidators: true
