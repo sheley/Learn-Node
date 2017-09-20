4fa8a7e617ba896c9582bcf2c27cae18b5253b56
Michele Guido <michele.guido@ada.com>
2017-09-20 12:12:02 +0200

    Through video 23.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -65,9 +65,13 @@ exports.getStoreBySlug = async (req, res, next) => {
 }
 
 exports.getStoresByTag = async (req, res, next) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag
-  res.render('tag', { tags, title: 'Tags', tag })
+  const tagQuery = tag || { $exists: true }; // either search for that tag or that store has a tag
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise])
+  res.render('tag', { tags, title: 'Tags', tag, stores });
 }
 
 exports.editStore = async  (req, res) => {
